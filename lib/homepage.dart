32868c54ad94ce83ab37b049eac145513b8f2386
Hyeongjin Lee <lhjin0j@gmail.com>
2023-01-02 01:55:48 +0900

    Bottom Navigation Bar Develop

---
+++ lib/homepage.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  HomePageState createState() => HomePageState();
+}
+
+class HomePageState extends State<HomePage> {
+  int selectedIndex = 0;
+  final widgetOptions = [
+    const Text('Home'),
+    const Text('Alarm'),
+    const Text('TimeSchedule'),
+    const Text('Calendar'),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFF383F51),
+      body: Center(
+        child: widgetOptions.elementAt(selectedIndex),
+      ),
+      bottomNavigationBar: Container(
+        decoration: const BoxDecoration(
+          color: Colors.white,
+          border: Border(top: BorderSide(color: Colors.white, width: 0.5))
+        ),
+        child: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          backgroundColor: const Color(0xFF383F51),
+          showSelectedLabels: false,
+          showUnselectedLabels: false,
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: "home"),
+            BottomNavigationBarItem(icon: Icon(Icons.schedule), label: "schedule"),
+            BottomNavigationBarItem(icon: Icon(Icons.today), label: "today"),
+            BottomNavigationBarItem(icon: Icon(Icons.calendar_month), label: "calendar"),
+          ],
+          currentIndex: selectedIndex,
+          selectedItemColor: Colors.white,
+          unselectedItemColor: Colors.grey,
+          onTap: onItemTapped,
+        ),
+      )
+    );
+  }
+
+  void onItemTapped(int index){
+    setState(() {
+      selectedIndex = index;
+    });
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:planner/homepage.dart';
 
 void main() {
-  runApp();
+  runApp(const MyApp());
 }
 
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "MyPlanner",
+      theme: ThemeData(
+        primaryColor: Colors.blue,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
